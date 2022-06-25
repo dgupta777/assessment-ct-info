a158836517c1f0dcf33120002182bca359e44e3b
dgupta777 <darshitgupta777@gmail.com>
2022-06-25 22:24:15 +0530

    feat: implement new-message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Material Modules
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 // Services
 
@@ -24,9 +30,16 @@ import { NewMessageComponent } from './new-message/new-message.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
     MatTableModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/new-message/new-message.component.ts
+++ src/app/new-message/new-message.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-message',
   templateUrl: './new-message.component.html',
-  styleUrls: ['./new-message.component.scss']
+  styleUrls: ['./new-message.component.scss'],
 })
 export class NewMessageComponent implements OnInit {
+  messageForm!: FormGroup;
+  loading = false;
+  errMsg = '';
+  @ViewChild('mForm') messageFormDirective: any;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm() {
+    this.messageForm = this.fb.group({
+      title: ['', Validators.required],
+      launchDate: [new Date(), Validators.required],
+      author: ['', Validators.required],
+      imageLink: '',
+      desc: '',
+    });
+  }
+
+  onSubmit() {
+    this.loading = true;
+    console.log(this.messageForm.value);
+    setTimeout(() => {
+      this.loading = false;
+      this.openSnackBar();
+      this.messageForm.reset({
+        title: '',
+        launchDate: new Date(),
+        author: '',
+        imageLink: '',
+        time: '',
+        desc: '',
+      });
+      this.messageFormDirective.resetForm();
+      this.router.navigate(['/']);
+    }, 3000);
+  }
+
+  openSnackBar() {
+    this.snackBar.open('Message sent successfully.', '', {
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      duration: 3000,
+      panelClass: ['snakeBarClass'],
+    });
+  }
 }
